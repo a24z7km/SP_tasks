eb88910468dbe437de9afc802fae2f51935ed2ce
a24z7km <a24z7km@aiit.ac.jp>
2025-06-13 10:17:03 +0900

    work_c

---
+++ first_last.py
@@ -0,0 +1,11 @@
+s1 = "spring"
+s2 = s1[:2] + s1[-2:]
+print(s2)
+
+s1 = "hello"
+s2 = s1[:2] + s1[-2:]
+print(s2)
+
+s1 = "abc"
+s2 = s1[:2] + s1[-2:]
+print(s2)
---
+++ work_c.py
@@ -0,0 +1,13 @@
+s1 = "The quick brown fox jumps over the lazy dog."
+
+print(s2) # The
+
+
+print(s2) # fox
+
+
+print(s2) # quick dog
+
+
+print(s2) # The lazy brown fox.
+
